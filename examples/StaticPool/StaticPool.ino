141f050f03a39f1b70ff7f14ba405afd9970daf2
David Rifkind <drifkind@acm.org>
2018-09-13 22:40:14 -0700

    Add StaticPool example.

---
+++ examples/StaticPool/StaticPool.ino
@@ -0,0 +1,36 @@
+//
+// StaticPool.ino
+//
+// Sample program for QDispatch library.
+//
+// 11 Sep 2018 <drifkind@acm.org>
+//
+
+// This example contains a ContextPool implementation that allocates a
+// fixed block of memory at compile time, so you can use a context pool
+// without the evil of dynamic allocation (if it is evil)
+
+#include <QDispatch.h>
+#include "StaticContextPool.h"
+
+#define POOL_SIZE 5
+StaticContextPool<POOL_SIZE> contextPool;
+TaskDispatcher dispatcher(&contextPool);
+
+# The rest of this is just the "QBlink" sample.
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  dispatcher.callEvery(500, blinkTask);
+}
+
+void loop()
+{
+  dispatcher.run();
+}
+
+void blinkTask()
+{
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
